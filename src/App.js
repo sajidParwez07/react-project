649571804b595cc0592f650a4bea8af7c5f30564
sajidParwez07 <136393992+sajidParwez07@users.noreply.github.com>
2023-06-19 00:24:53 +0530

    i change in App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div>Name : {data.name}</div>
+    <div>Email : {data.email}</div>
+    <div>Website : {data.website}</div>
+    <div><label>Country :</label>
+    <select>
+      {data.country.map((country) => {
+        return <option key={country.id} value={country.id}>{country.name}</option>
+      })}
+    </select>
     </div>
+  </div>
   );
 }
 
